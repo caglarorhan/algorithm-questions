0b7f4d81cd1d833ed8955775b393a1b001b40934
caglaror <caglaror@gmail.com>
2019-04-02 22:49:43 -0700

    anagram sample shows frequency usage on objects

---
+++ correctAnagram.js
@@ -0,0 +1,18 @@
+const anagram = (str1, str2) => {
+    if(str1.length!==str2.length) return false;
+    const obj1 = {};
+    const obj2 = {};
+    for(let i = 0; i<str1.length; i++){
+        obj1[str1[i]] = ++obj1[str1[i]] || 1;
+        obj2[str2[i]] = ++obj2[str2[i]] || 1;
+    }
+    for(let key in obj1){
+        //console.log(key)
+        if(obj1[key]!==obj2[key]) return false;
+    }
+return true;
+};
+
+
+console.log(anagram('texttwisttime', 'timetwisttext'));
+console.log(anagram('ey edip adanada pide ye', 'ey edip adanada pide ye'));
