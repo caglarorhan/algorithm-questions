4944fa7eaebf50673f50bf7bd37a0a89f196cbcb
caglaror <caglaror@gmail.com>
2019-04-05 18:18:34 -0700

    debug function calls :)

--- factorialWithRecursion.js
+++ factorialWithRecursion.js
@@ -1,9 +1,8 @@
 function factorial(n){
-    if(n==0) return 1;
-    if(n==1) return 1;
+    if(n===0) return 1;
     return (n * factorial(n-1));
 }
 
-console.log(5);
-console.log(6);
-console.log(7);
+console.log(factorial(5));
+console.log(factorial(6));
+console.log(factorial(7));
